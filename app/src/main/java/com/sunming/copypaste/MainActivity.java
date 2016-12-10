ce95282c6484225f69868fecbfe4792aac6569cd
Minkyujo <minkyujo@irm.kr>
2016-12-10 22:03:21 +0900

    DB셋팅

--- app/src/main/java/com/sunming/copypaste/MainActivity.java
+++ app/src/main/java/com/sunming/copypaste/MainActivity.java
@@ -1,13 +1,18 @@
 package com.sunming.copypaste;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MySQLiteHandler ms = new MySQLiteHandler(this);
+        Log.e("matthew","as");
+        ms.insertMyText("123","456");
+        ms.getMyText();
     }
 }
